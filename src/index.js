eaca32affe883119265ce562f30e805244a6e16d
Vladyslav Rohalov <domus.lg@gmail.com>
2022-12-23 13:26:58 +0100

    js mod
    Creating number-animation.js
    Changing time  animation
    Changing throttle time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// Throttle
-//------------------------------------------------------------------------------------
-const throttle = require('lodash.throttle');
-
 //Swiper
 //-------------------------------------------------------------------------------------
 import './js/header-scroll';
@@ -139,78 +135,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //--------------------------------------------------------------------------------------
 // Numbers animation
-
-const refs = {
-  animationEl: document.querySelector('.advantages__text'),
-  numberPossFirst: document.querySelector('[data-number="1"]'),
-  numberPossSecond: document.querySelector('[data-number="2"]'),
-  numberPossThird: document.querySelector('[data-number="3"]'),
-};
-
-const animationElHeight = refs.animationEl.offsetHeight;
-const animationElOffest = offset(refs.animationEl).top;
-
-// console.log(
-//   `Для анимации необходимо проскролить: ${
-//     animationElOffest - window.innerHeight + animationElHeight - 1
-//   }`
-// );
-
-function offset(el) {
-  const rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
-}
-
-const throttledOnScrollMouse = throttle(onScrollMouse, 1000);
+import throttledOnScrollMouse from './js/number-animation';
 
 window.addEventListener('scroll', throttledOnScrollMouse);
-
-function onScrollMouse() {
-  // console.log(`Проскролил px - ${scrollY}`);
-  if (
-    scrollY <
-    animationElOffest - window.innerHeight + animationElHeight - 1
-  ) {
-    return;
-  }
-  refs.animationEl.classList.add('isActive');
-  changeNumberFirst();
-  changeNumberSecond();
-  changeNumberThird();
-  window.removeEventListener('scroll', throttledOnScrollMouse);
-}
-
-function changeNumberFirst() {
-  let counter = 0;
-  const timerId = setInterval(() => {
-    if (counter === 721) {
-      clearInterval(timerId);
-    }
-    refs.numberPossFirst.textContent = counter;
-    counter += 7;
-  }, 50);
-}
-
-function changeNumberSecond() {
-  let counterSecond = 0;
-  const timerId = setInterval(() => {
-    if (counterSecond === 16) {
-      clearInterval(timerId);
-    }
-    refs.numberPossSecond.textContent = counterSecond + 'kg';
-    counterSecond += 1;
-  }, 310);
-}
-
-function changeNumberThird() {
-  let counterThird = 0;
-  const timerId = setInterval(() => {
-    if (counterThird === 84) {
-      clearInterval(timerId);
-    }
-    refs.numberPossThird.textContent = counterThird;
-    counterThird += 1;
-  }, 60);
-}
---
+++ src/js/number-animation.js
@@ -0,0 +1,78 @@
+const throttle = require('lodash.throttle');
+
+// Numbers animation
+
+const refs = {
+  animationEl: document.querySelector('.advantages__text'),
+  numberPossFirst: document.querySelector('[data-number="1"]'),
+  numberPossSecond: document.querySelector('[data-number="2"]'),
+  numberPossThird: document.querySelector('[data-number="3"]'),
+};
+
+const animationElHeight = refs.animationEl.offsetHeight;
+const animationElOffest = offset(refs.animationEl).top;
+
+// console.log(
+//   `Для анимации необходимо проскролить: ${
+//     animationElOffest - window.innerHeight + animationElHeight - 20
+//   }`
+// );
+
+function offset(el) {
+  const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
+
+export const throttledOnScrollMouse = throttle(onScrollMouse, 500);
+
+window.addEventListener('scroll', throttledOnScrollMouse);
+
+function onScrollMouse() {
+  // console.log(`Проскролил px - ${scrollY}`);
+  if (
+    scrollY <
+    animationElOffest - window.innerHeight + animationElHeight - 20
+  ) {
+    return;
+  }
+  refs.animationEl.classList.add('isActive');
+  changeNumberFirst();
+  changeNumberSecond();
+  changeNumberThird();
+  window.removeEventListener('scroll', throttledOnScrollMouse);
+}
+
+function changeNumberFirst() {
+  let counter = 0;
+  const timerId = setInterval(() => {
+    if (counter === 721) {
+      clearInterval(timerId);
+    }
+    refs.numberPossFirst.textContent = counter;
+    counter += 7;
+  }, 35);
+}
+
+function changeNumberSecond() {
+  let counterSecond = 0;
+  const timerId = setInterval(() => {
+    if (counterSecond === 16) {
+      clearInterval(timerId);
+    }
+    refs.numberPossSecond.textContent = counterSecond + 'kg';
+    counterSecond += 1;
+  }, 217);
+}
+
+function changeNumberThird() {
+  let counterThird = 0;
+  const timerId = setInterval(() => {
+    if (counterThird === 84) {
+      clearInterval(timerId);
+    }
+    refs.numberPossThird.textContent = counterThird;
+    counterThird += 1;
+  }, 42);
+}
